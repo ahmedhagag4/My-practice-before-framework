e70bf5d1e3c4257a2006dd92f2511ae698e87cd5
Ahmed_Hagag <145713187+ahmedhagag4@users.noreply.github.com>
2026-04-26 00:21:39 +0300

    Delete Modules directory

--- Modules/src/JavaBase/ExplainTheImage.java
+++
@@ -1,8 +0,0 @@
-package JavaBase;
-
-public class ExplainTheImage {
-
-    //JavaBase is the module and it has list of packages
-    //packages in blue is can be used outside the module (exported)
-    //packages in red in only can be used inside the module (concealed)
-}
